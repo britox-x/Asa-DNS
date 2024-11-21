8363f3ff22aaa6a704e33d79e3a676403ed3eded
Matheus Brito Bezerra Cavalcanti <matheus.britobc61@gmail.com>
2024-11-21 20:33:16 -0300

    Descrição das alterações realizadas nos arquivos build.sh e Dockerfile

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-# Construir a imagem para o servidor Web (Nginx)
-docker build -t asa-dns-web ./WEB
+# Build do DNS
+echo "[*] Construindo a imagem do servidor DNS..."
+docker build -t dns-server -f Dockerfile.DNS .
 
-# Construir a imagem para o servidor DNS (BIND9)
-docker build -t asa-dns-dns ./DNS
+# Remove contêiner DNS antigo (se existir)
+docker rm -f dns-server 2>/dev/null
 
-# Rodar os contêineres
-docker run -d -p 80:80 --name web-server asa-dns-web
-docker run -d -p 53:53/udp --name dns-server asa-dns-dns
+# Rodar contêiner DNS
+echo "[*] Iniciando o servidor DNS..."
+docker run --name dns-server -d --network bridge -p 53:53/udp -p 53:53/tcp dns-server
+
+# Build do Web
+echo "[*] Construindo a imagem do servidor Web..."
+docker build -t web-server -f Dockerfile.Web .
+
+# Remove contêiner Web antigo (se existir)
+docker rm -f web-server 2>/dev/null
+
+# Rodar contêiner Web
+echo "[*] Iniciando o servidor Web..."
+docker run --name web-server -d --network bridge -p 8080:80 web-server
+
+# Verificar contêineres em execução
+docker ps
