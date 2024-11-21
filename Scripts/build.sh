9b0c827eadb530d33ca5f51044691c7cc982c0e5
Matheus Brito Bezerra Cavalcanti <matheus.britobc61@gmail.com>
2024-11-21 19:59:38 -0300

    atualizações

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-docker build -t asa-dns .
-docker run -d -p 53:53/udp -p 53:53/tcp --name asa-dns-container asa-dns
+
+# Construir a imagem para o servidor Web (Nginx)
+docker build -t asa-dns-web ./WEB
+
+# Construir a imagem para o servidor DNS (BIND9)
+docker build -t asa-dns-dns ./DNS
+
+# Rodar os contêineres
+docker run -d -p 80:80 --name web-server asa-dns-web
+docker run -d -p 53:53/udp --name dns-server asa-dns-dns
